5bfcb5dcd7040a7254c742bd2ba6dd88e32413e9
Ayaz <camelot1402@rambler.ru>
2020-08-18 00:13:37 +0400

    5. refactoring

---
+++ app/src/main/java/com/example/telegram/ui/fragments/ChatFragment.kt
@@ -0,0 +1,12 @@
+package com.example.telegram.ui.fragments
+
+import com.example.telegram.R
+import com.example.telegram.databinding.FragmentChatBinding
+
+class ChatFragment : BaseFragment(R.layout.fragment_chat) {
+    private lateinit var mBinding: FragmentChatBinding
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.telegram.ui.fragments
+
+import android.os.Bundle
+import com.example.telegram.R
+import com.example.telegram.databinding.FragmentSettingsBinding
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+    private lateinit var mBinding: FragmentSettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
